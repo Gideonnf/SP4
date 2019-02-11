f76c95e34758975e5870e6e63568979f2985f317
JamesCrr <g0546699m@gmail.com>
2019-02-12 00:04:40 +0800

    rename script

--- SP4_Unity_Project/Assets/Scripts/Network_Related/NetworkManagerExternalScript.cs
+++ SP4_Unity_Project/Assets/Scripts/Network_Related/NetworkManagerExternalScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class NetworkManagerExternal_ServerScript : NetworkManager//MonoBehaviour
+public class NetworkManagerExternalScript : NetworkManager//MonoBehaviour
 {
     [SerializeField]
     private List<NetworkConnection> connectedClients = new List<NetworkConnection>();
