e518bc26b25b1f1bc9efbd34b7e74eea12201da6
JamesCrr <g0546699m@gmail.com>
2019-02-11 11:08:42 +0800

    Added basic Player Spawning when connected to server

---
+++ SP4_Unity_Project/Assets/Scripts/Network_Related/StartingPlayerScrpt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingPlayerScrpt : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerPrefabToInstantiate = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // if there is a Prefab to create...
+        if (playerPrefabToInstantiate)
+        {
+            // Since the PlayerHandler is an empty gameObject and not currently controlled
+            // By the player, Give the player something phyiscal to move around!!!
+            Debug.Log("Creating PlayerPrefab...");
+            Instantiate(playerPrefabToInstantiate);
+        }
+        else
+        {
+            Debug.Log("FAILED TO CREATE PLAYER PREFAB!!!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
