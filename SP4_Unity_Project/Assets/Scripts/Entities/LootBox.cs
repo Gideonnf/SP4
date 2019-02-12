cbac92912cb68269e3090f104c9cb5153a44f03e
gideon <gideonnfrancis999@gmail.com>
2019-02-13 02:35:09 +0800

    did lootbox spin and rotate

--- SP4_Unity_Project/Assets/Scripts/Entities/LootBox.cs
+++ SP4_Unity_Project/Assets/Scripts/Entities/LootBox.cs
@@ -8,12 +8,21 @@ public class LootBox : NetworkBehaviour
     public bool alive = true;
 
     public GameObject powerupPrefab;
+    private Rigidbody rb;
 
     public float offset = 2.0f;
+    public float rotateSpeed = 5f;
+    private Vector3 m_EulerAngleVelocity;
     private float maxY, minY;
     private static bool floatDown = false;
-    
-   // [ClientCallback]
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        m_EulerAngleVelocity = new Vector3(0, 25, 0);
+    }
+
+    // [ClientCallback]
     public override void OnStartClient()
     {
         if (!ServerManager.serverManager.Lootboxes.Contains(this.gameObject))
@@ -33,16 +42,33 @@ public class LootBox : NetworkBehaviour
 
     public void UpdateLootbox()
     {
+
+    }
+
+    public void SetUp(Vector3 Position)
+    {
+        maxY = Position.y + offset;
+        minY = Position.y - offset;
+    }
+
+    void Update()
+    {
+        // this.transform.Translate(0, 0.1f, 0);
         // Lootbox.transform.Translate(0, 0.1f, 0);
         // Lootbox.GetComponent<NetworkTransform>().SetDirtyBit(1);
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+        //transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime); 
+
         if (floatDown == false)
         {
-           // this.transform = 
-            this.transform.Translate(0, 2.0f * Time.deltaTime, 0);
+            // this.transform = 
+            rb.MovePosition(transform.position + transform.up * Time.deltaTime);
+            // this.transform.Translate(0, 2.0f * Time.deltaTime, 0);
         }
         else
         {
-            this.transform.Translate(0, -2.0f * Time.deltaTime, 0);
+            rb.MovePosition(transform.position - transform.up  * Time.deltaTime);
         }
 
         if (transform.position.y >= maxY)
@@ -55,17 +81,6 @@ public class LootBox : NetworkBehaviour
         }
 
         GetComponent<NetworkTransform>().SetDirtyBit(1);
-    }
-
-    public void SetUp(Vector3 Position)
-    {
-        maxY = Position.y + offset;
-        minY = Position.y - offset;
-    }
-
-    void Update()
-    {
-       // this.transform.Translate(0, 0.1f, 0);
     }
 
     void OnTriggerEnter(Collider other)
