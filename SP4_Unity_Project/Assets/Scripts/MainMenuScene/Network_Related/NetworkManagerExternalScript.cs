7c1e1345a4a75bce241f4240c25bae5230a2ae3b
JamesCrr <g0546699m@gmail.com>
2019-02-12 16:29:32 +0800

    added custom NetworkManager so can customize UI

--- SP4_Unity_Project/Assets/Scripts/MainMenuScene/Network_Related/NetworkManagerExternalScript.cs
+++ SP4_Unity_Project/Assets/Scripts/MainMenuScene/Network_Related/NetworkManagerExternalScript.cs
@@ -1,66 +1,100 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class NetworkManagerExternalScript : NetworkManager//MonoBehaviour
 {
-    [SerializeField]
-    private List<NetworkConnection> connectedClients = new List<NetworkConnection>();
+    //[SerializeField]
+    //private List<NetworkConnection> connectedClients = new List<NetworkConnection>();
 
 
-    // When a new client tries to connect to this server
-    // THIS IS ONLY EXECUTED IN THE SERVER
-    public override void OnServerConnect(NetworkConnection newConnection)
+    //// When a new client tries to connect to this server
+    //// THIS IS ONLY EXECUTED IN THE SERVER
+    //public override void OnServerConnect(NetworkConnection newConnection)
+    //{
+    //    //if (Conn.hostId >= 0)
+    //    //{
+    //        Debug.Log("New Player has joined of IP: " + newConnection.address);
+    //    //}
+
+    //    connectedClients.Add(newConnection);
+    //}
+
+    //// When an existing client wants to disconnect from server
+    //// THIS IS ONLY EXECUTED IN THE SERVER
+    //public override void OnServerDisconnect(NetworkConnection conn)
+    //{
+    //    // Your code here
+    //    Debug.Log("Player has LEFT");
+
+    //    foreach (NetworkConnection connection in connectedClients)
+    //    {
+    //        if(conn == connection)
+    //        {
+    //            //ClientLeft(connection.address);
+    //            Debug.Log("Delete Leaving Client's Stuff");
+    //            NetworkServer.DestroyPlayersForConnection(conn);
+    //        }
+    //    }
+    //}
+
+
+    public GameObject StartHostButton;
+    public GameObject JoinGameButton;
+
+    public GameObject IpAddress;
+
+    public void StartUpHost()
     {
-        //if (Conn.hostId >= 0)
-        //{
-            Debug.Log("New Player has joined of IP: " + newConnection.address);
-        //}
-
-        connectedClients.Add(newConnection);
+        SetPort();
+        NetworkManager.singleton.StartHost();
     }
 
-    // When an existing client wants to disconnect from server
-    // THIS IS ONLY EXECUTED IN THE SERVER
-    public override void OnServerDisconnect(NetworkConnection conn)
+    public void JoinGame()
     {
-        // Your code here
-        Debug.Log("Player has LEFT");
+        SetIpAddress();
+        SetPort();
 
-        foreach (NetworkConnection connection in connectedClients)
+        NetworkManager.singleton.StartClient();
+    }
+
+
+    void SetIpAddress()
+    {
+        string ipAddress = IpAddress.transform.Find("Text").GetComponent<Text>().text;
+        NetworkManager.singleton.networkAddress = ipAddress;
+    }
+    void SetPort()
+    {
+        NetworkManager.singleton.networkPort = 7777;
+    }
+
+
+    private void OnLevelWasLoaded(int level)
+    {
+        // 0 is main menu here in build settingss
+        if(level == 0)
         {
-            if(conn == connection)
-            {
-                //ClientLeft(connection.address);
-                Debug.Log("Delete Leaving Client's Stuff");
-                NetworkServer.DestroyPlayersForConnection(conn);
-            }
+            //SetUpMenuSceneButtons();
+            StartCoroutine(SetUpMenuSceneButtons());
         }
+       
     }
 
 
-    //Detect when a client connects to the Server
-    //[ClientRpc]
-    //public override void OnClientConnect(NetworkConnection connection)
-    //{
-    //    Debug.Log("A Client Connected");
-    //}
+    IEnumerator SetUpMenuSceneButtons()
+    {
+        // Prevent adding listener to old NetWorkManager
+        yield return new WaitForSeconds(0.3f);
 
+        // clear and reput event listeners if scene is reloaded
+        StartHostButton.GetComponent<Button>().onClick.RemoveAllListeners();
+        StartHostButton.GetComponent<Button>().onClick.AddListener(StartUpHost);
 
-    //Detect when a client connects to the Server
-    //This is called on the client when it disconnects from the server
-    //[ClientRpc]
-    //public override void OnClientDisconnect(NetworkConnection connection)
-    //{
-    //    Debug.Log("A Client Disconnected");
-    //}
-
-    // [ClientRpc] makes sures that the Function will ONLY BE 
-    // Executed on the Clients.
-    //[ClientRpc]
-    //void ClientLeft(string clientIP)
-    //{
-    //    Debug.Log("Delete Leaving Client's Stuff");
-    //}
+        JoinGameButton.GetComponent<Button>().onClick.RemoveAllListeners();
+        JoinGameButton.GetComponent<Button>().onClick.AddListener(JoinGame);
+    }
 }
