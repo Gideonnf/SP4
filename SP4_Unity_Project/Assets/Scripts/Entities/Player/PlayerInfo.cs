439d9ce2153ab05ef925a94699354e0f6c409b9b
Jordan Tan Yan Boon <31056129+174561B@users.noreply.github.com>
2019-02-13 11:57:51 +0800

    Client side zone

---
+++ SP4_Unity_Project/Assets/Scripts/Entities/Player/PlayerInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    public Material good;
+    public Material bad;
+    public GameObject zone;
+
+    // Start is called before the first frame update
+    void Start()
+    {      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(zone.GetComponent<ZoneController>().GetPos(), this.transform.position) > zone.GetComponent<ZoneController>().GetScale().x / 2)
+        {
+            if (GetComponent<Renderer>().material != bad)
+            {
+                GetComponent<Renderer>().material = bad;
+            }
+        }
+        else
+        {
+            if (GetComponent<Renderer>().material != good)
+            {
+                GetComponent<Renderer>().material = good;
+            }
+        }
+    }
+}
---
+++ SP4_Unity_Project/Assets/Scripts/Entities/ZoneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (this.transform.localScale.x > 2)
+        //    this.transform.localScale -= new Vector3(0.05F, 0, 0.05F);
+    }
+
+    public Vector3 GetScale()
+    {
+        return this.transform.localScale;
+    }
+
+    public Vector3 GetPos()
+    {
+        return this.transform.position;
+    }
+}
